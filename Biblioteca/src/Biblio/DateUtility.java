011e54a7a132d854b5ad493f0b412ec6f7b21fbe
marti90 <debernardi.martina@gmail.com>
2016-10-04 14:19:54 +0200

    DateUtility

--- Biblioteca/src/Biblio/DateUtility.java
+++ Biblioteca/src/Biblio/DateUtility.java
@@ -2,9 +2,13 @@ package Biblio;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
+
 public class DateUtility {
+	
+	private static final int NUMERO_MILLISECONDI_PER_UN_GIORNO = 86400000;
 	
 	public Date convertStringtoDate(String dataString) throws ParseException{
 		
@@ -14,12 +18,42 @@ public class DateUtility {
 		return date;
 	}
 	
-    public java.sql.Date convertJavaDateToSqlDate(Date date){
+    public java.sql.Date convertJavaDateToSqlDate(java.util.Date date){
     	
     	java.sql.Date dSql = new java.sql.Date(date.getTime());
     	
     	return dSql;
 		
 	}
+    
+    public java.util.Date convertSqlDateToJavaDate(java.sql.Date date){
+    	
+    	java.util.Date dUtil = new java.util.Date(date.getTime());
+    	
+    	return dUtil;
+		
+	}
+    
+    public Date convertCalendarToDate(Calendar c){
+    	
+    	return c.getTime();
+    }
+    
+    //Convertire java.util.Date in Calendar
+    public Calendar convertDateToCalendar(java.util.Date d){
+    	
+    	Calendar cal= Calendar.getInstance();
+    	cal.setTimeInMillis(d.getTime());
+    	return cal;
+    }
+    
+    public long getDayDifference(Date date1, Date date2){
+    	
+    	long diffMillisecondi= date2.getTime()-date1.getTime();
+    	long dayDiff=diffMillisecondi/NUMERO_MILLISECONDI_PER_UN_GIORNO;
+    	
+    	return dayDiff;
+    	
+    }
 
 }
